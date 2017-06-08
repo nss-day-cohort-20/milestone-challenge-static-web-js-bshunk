29de42db3c235f66bd33c6b475f16d0cb7472632
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-06-08 15:39:35 -0500

    update index and JS, plus add css

--- main.js
+++ main.js
@@ -1,40 +1,12 @@
 console.log("Ahoy, mate!");
 
-let character = "!", "@", "#", "$", "%", "^", "&", "*", "(", ")";
-let height = "1", "2","3","4","5","6","7","8","9","10";
+let character = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+let height = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
-let colors = ["red", "blue", "green", "yellow", "orange", "teal"];
-let numbers = [20,10,5,8,30,100,19];
-
-let reversedColors = colors.reverse()
-console.log("oooh, pretty", reversedColors);
-
-let sortedColors = colors.sort();
-console.log("sorted", sortedColors);
-
-// BAD WAY TO SORT NUMBERS, ONLY SORTS THE FIRST CHARACTER//
-let sortaSorted = numbers.sort();
-console.log("uhhhh", sortaSorted);
-
-let sortedNums = numbers.sort(function(first, second){
-	return first - second;
-});
-console.log("sortedNums", sortedNums);
-
-let joinedColors = colors.join(", ");
-console.log("joined", joinedColors);
-
-// SLICE CUTS FROM THE BEGINNING OF THE ARRAY YOU CHOOSE TO THE BEFORE POSITION OF THE CHARACTER AFTER THE DESIRED ONE//
-let fruits = ["banana", "orange", "lemon", "apple", "mango"];
-let citrus = fruits.slice(1, 3);
-// console.log("fruits", fruits);
-console.log("cirtus?", citrus);
-
-
-// var colors = ["red", "blue", "green", "yellow", "orange", "teal"];
-
-colors.forEach(function(color) {
-	console.log(`I like the color ${color}`);
-});
+function makeTree() {
+	for (i = 0; i >= 10; i++) {
+		console.log(character)
+	}
+};
 
 
