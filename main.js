9eb5414ada941f139b8af56cc0d63b1dc96c9ed3
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-06-09 12:47:52 -0500

    work on JS Challenge

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 console.log("Ahoy, mate!");
 
-let character = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
-let height = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
-function makeTree() {
-	for (i = 0; i >= 10; i++) {
-		console.log(character)
-	}
-};
+let height = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let firstInput = `height`;
+console.log(firstInput);
+
 
 
