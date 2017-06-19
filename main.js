d8bcd9fc7fc18cb97744a697f36a122684e27800
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-06-19 13:13:20 -0500

    Work on JS quiz

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 console.log("Ahoy, mate!");
 
-
-let height = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-let firstInput = `height`;
-console.log(firstInput);
+var height = document.getElementsbyClassName(height);
+var character = document.getElementsbyClassName(character);	
+console.log(height);
 
 
 
